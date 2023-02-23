be8d05ad70c803415fe39eb84e967559fd59189c
omer hadad <omer.hadad1@gmail.com>
2023-02-23 05:37:02 +0200

    started gui

---
+++ QuoridorApp/Form1.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace QuoridorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            AddWalls();
+            InitializeComponent();
+        }
+
+
+
+        private void CanPlaceVerticalWall(object sender, MouseEventArgs e)
+        {
+            // TODO: creating temp array of walls that only the legal walls are in
+            // check if the mouse is over the control
+            for(int i = 0; i < walls.Length; i++)
+            {
+                bool placeWall= i>63 ? (e.Y >= walls[i].Location.Y && e.Y <= walls[i].Location.Y + walls[i].Height && e.X >= walls[i].Location.X && e.X <= walls[i].Location.X + walls[i].Width/2) :
+                                       (e.Y >= walls[i].Location.Y && e.Y <= walls[i].Location.Y + walls[i].Height/2 && e.X >= walls[i].Location.X && e.X <= walls[i].Location.X + walls[i].Width);
+                walls[i].Visible = placeWall;
+            }
+        }
+
+
+        private void userPawn_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
